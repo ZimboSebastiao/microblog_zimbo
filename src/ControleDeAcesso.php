3675410d9b1dc117b8e893dcfc412c44412ab6d9
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-10-17 08:57:02 -0300

    Update ControleDeAcesso.php

--- src/ControleDeAcesso.php
+++ src/ControleDeAcesso.php
@@ -11,15 +11,17 @@ final final class ControleDeAcesso {
             session_start();
         }
 
-
- 
     }
 
+
     public function verificaAcesso():void{
+        // Se não existir uma variável de sessão chamada "id" (ou seja, ainda não houve um login por parte do usuário)
         if(!isset($_SESSION['id'])){
+
+            // então destrua qualquer resquício de sessão, redirecione para o formulário de login e pare completamente o script
             session_destroy();
             header("location:../login.php");
-            die();
+            die(); // ou exit;
         }
     }
 
