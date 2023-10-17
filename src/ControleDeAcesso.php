528d405b8288b1afec57e68db294ce0283eb2380
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-10-17 09:26:56 -0300

    Update ControleDeAcesso.php

--- src/ControleDeAcesso.php
+++ src/ControleDeAcesso.php
@@ -26,6 +26,12 @@ final class ControleDeAcesso {
     }
 
 
+    public function login(int $id, string $nome, string $tipo):void{
+        // No momento em que ocorre o login, criamos variáveis de sessão contendo os dados que queremos monitorar atráves da sessão enquanto a pessoa estiver logada
+        $_SESSION["id"] = $id;
+        $_SESSION["nome"] = $nome;
+        $_SESSION["tipo"] = $tipo;
+    }
 
 
 
