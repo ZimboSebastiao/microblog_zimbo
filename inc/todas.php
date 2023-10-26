6c233827d2c856460eb81fe55dd8e561ae43f0e4
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-10-26 11:11:50 -0300

    update

---
+++ inc/todas.php
@@ -0,0 +1,23 @@
+<?php
+use Microblog\Utilitarios;
+// Utilitarios::dump($noticia);
+$todasNoticias = $noticia->listarTodas();
+?>
+
+<hr class="my-5 w-50 mx-auto">
+        
+
+        <div class="row my-1">
+            <div class="col-12 px-md-1">
+                <div class="list-group">
+                    <h2 class="fs-6 text-center text-muted">Todas as notícias</h2>
+                    <?php foreach ($todasNoticias as $todas) {?>
+                    <a href="noticia.php" class="list-group-item list-group-item-action">
+                         <h3 class="fs-6"><time><?=$todas["data"]?></time> - <?=$todas["titulo"]?></h3>
+                        <p><?=$todas["resumo"]?></p>
+                    </a>
+                    <?php } ?>
+                    
+                </div>
+            </div>
+        </div>
