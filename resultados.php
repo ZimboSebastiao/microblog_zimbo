2dbe2f7b122b65d79e30db38bcab6b6f788de7b0
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-10-27 10:47:10 -0300

    Update resultados.php

--- resultados.php
+++ resultados.php
@@ -5,7 +5,7 @@ use Microblog\Utilitarios;
 require_once "inc/cabecalho.php";
 $noticia->setTermo($_GET["busca"]);
 $resultados = $noticia->busca();
-Utilitarios::dump($resultados);
+// Utilitarios::dump($resultados);
 
 ?>
 
@@ -13,36 +13,25 @@ Utilitarios::dump($resultados);
 <div class="row bg-white rounded shadow my-1 py-4">
     <h2 class="col-12 fw-light">
         Você procurou por <span class="badge bg-dark"><?= $noticia->getTermo();?></span> e
-        obteve <span class="badge bg-info">X</span> resultados
+        obteve <span class="badge bg-info"><?= count($resultados)?></span> resultados
     </h2>
     
+    <?php foreach ($resultados as $itemResultados) {?>
     <div class="col-12 my-1">
         <article class="card">
             <div class="card-body">
-                <h3 class="fs-4 card-title fw-light">Título da notícia...</h3>
+                <h3 class="fs-4 card-title fw-light"><?=$itemResultados["titulo"]?></h3>
                 <p class="card-text">
-                    <time>Data da notícia</time> - 
-                    Resumo da notícia.
+                    <time><?=$itemResultados["data"]?></time> - 
+                    <?=$itemResultados["resumo"]?>.
                 </p>
                 
-                <a href="noticia.php" class="btn btn-primary btn-sm">Continuar lendo</a>
+                <a href="noticia.php?id=<?=$itemResultados["id"]?>" class="btn btn-primary btn-sm">Continuar lendo</a>
             </div>
         </article>
     </div>
+    <?php }?>
 
-    <div class="col-12 my-1">
-        <article class="card">
-            <div class="card-body">
-                <h3 class="fs-4 card-title fw-light">Título da notícia...</h3>
-                <p class="card-text">
-                    <time>Data da notícia</time> - 
-                    Resumo da notícia.
-                </p>
-                
-                <a href="noticia.php" class="btn btn-primary btn-sm">Continuar lendo</a>
-            </div>
-        </article>
-    </div>
 
 </div>        
 
